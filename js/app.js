d8b233096b2cc33713fc8bd787e1c559cd5311e4
mdsajedul <sajedulislms@gmail.com>
2021-09-01 11:52:44 +0600

    modal work properly in search

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ loadMeal()
 function ViewMeal(data){
     for(meal of data ){
         mealPushInPage(meal)
-    
     }
 }
 
@@ -33,30 +32,8 @@ function mealPushInPage(meal){
         <h5 class="card-title">${meal.strMeal}</h5>
         <p class="card-text">${meal.strInstructions.substr(0,95)}...</p>
         
-        <a id="see-more" data-bs-toggle="modal" data-bs-target="#see-more-leModal" class="btn btn-primary">See more</a>
+        <a id="see-more" onclick="mealDetails('${meal.strMeal}')" data-bs-toggle="modal" data-bs-target="#see-more-leModal" class="btn btn-primary">See more</a>
     </div>
-
-
-    
-    <div class="modal fade" id="see-more-leModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-    <div class=" modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="ModalLabel">${meal.strMeal}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            ...
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-        </div>
-    </div>
-    </div>
-
-
     `;
     mealContainer.appendChild(div);
 
@@ -88,3 +65,26 @@ document.getElementById('home-btn2').addEventListener('click',()=>{
     mealContainer.textContent='';
     loadMeal();
 })
+
+const mealDetails=(mealName)=>{
+    //console.log(mealName);
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+    fetch(url).then(res=>res.json().then(data=>mealDetailsInModal(data.meals[0])));
+        
+}
+
+const mealDetailsInModal=(meal)=>{
+
+    const mealTitle = document.getElementById('ModalLabel');
+    mealTitle.innerText='';
+    console.log(meal.strMeal)
+    mealTitle.innerText=meal.strMeal
+
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML=`
+    <img class="card-img-top mt-2 border rounded " src="${meal.strMealThumb}" alt="" height="200" width=100%>
+    <div class="card-body">
+        <p class="card-text">${meal.strInstructions}</p>
+    </div>
+    `;
+}
